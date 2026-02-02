e1984be24764b87c9edc9818b0b96e39fc36c23c
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2026-02-02 19:25:16 +0200

    Initial commit

---
+++ src/main/java/com/csvtool/csvsqltool/CsvOnlineToolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.csvtool.csvsqltool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvOnlineToolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvOnlineToolProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csvtool/csvsqltool/CsvOnlineToolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csvtool.csvsqltool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvOnlineToolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
